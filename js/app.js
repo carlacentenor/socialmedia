3cb2a3fc70811623eb2968e5e1dec4f911e50a6e
carlacentenor <carlacentenor@gmail.com>
2017-12-11 09:48:40 -0500

    guia de estilos

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
-var navMobile = document.querySelector('.nav-mobile');
-var menuMobile = document.querySelector('.menu-mobile-js');
-navMobile.addEventListener('click',function(){
-    if(menuMobile.classList.contains("disabled-menu")){
-        menuMobile.classList.remove("disabled-menu");
-        menuMobile.classList.add("enabled-menu");
-      }
-      else {
-      menuMobile.classList.add("disabled-menu");
-      menuMobile.classList.remove("enabled-menu");
-      }
+window.addEventListener('load', function() {
+  var navMobile = document.querySelector('.nav-mobile');
+  var menuMobile = document.querySelector('.menu-mobile-js');
+  navMobile.addEventListener('click', function() {
+    if (menuMobile.classList.contains('disabled-menu')) {
+      menuMobile.classList.remove('disabled-menu');
+      menuMobile.classList.add('enabled-menu');
+    } else {
+      menuMobile.classList.add('disabled-menu');
+      menuMobile.classList.remove('enabled-menu');
+    }
+  });
 });
-
-
 /*
 
 
